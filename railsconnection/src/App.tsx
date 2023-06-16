c92b626cfd76d0bf2d494ccc4e3c84c4fb1470bb
Kavipriya Thirumurugan <Kavipriya.Thirumurugan@maplelabs.com>
2023-06-16 14:54:05 +0530

    scss

---
+++ railsconnection/src/App.tsx
@@ -0,0 +1,69 @@
+import "./App.scss";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Todo } from "./components/Todo";
+import Button from "@mui/material/Button";
+
+function App() {
+  const [api, setApi] = useState([]);
+  const [show, setShow] = useState(false);
+
+  const API_URL = "http://localhost:3000/todos";
+
+  const submitTask = async (taskData: any) => {
+    console.log(taskData);
+
+    return axios
+      .post("http://localhost:3000/todos", taskData)
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
+  const deleteTask = async (taskId: any) => {
+    console.log(taskId);
+
+    return axios
+      .delete(`http://localhost:3000/todos/${taskId}`)
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
+  function visible() {
+    return setShow(!show);
+  }
+
+  function getAPIData() {
+    return axios.get(API_URL).then((response) => setApi(response.data));
+  }
+
+  return (
+    <div className="App">
+      <Button className="sendbtn"
+        onClick={() => {
+          getAPIData();
+          visible();
+        }}
+      >
+        {show ? "Close Todo App" : "Open Todo App"}
+      </Button>
+      {show && (
+        <Todo
+          api={api}
+          setApi={setApi}
+          submitTask={submitTask}
+          deleteTask={deleteTask}
+        />
+      )}
+    </div>
+  );
+}
+
+export default App;
---
+++ railsconnection/src/components/Todo.tsx
@@ -0,0 +1,53 @@
+import React, { useEffect, useState } from "react";
+import CancelIcon from "@mui/icons-material/Cancel";
+import SendIcon from '@mui/icons-material/Send';
+import { Button } from "@mui/material";
+import axios from "axios";
+
+export const Todo = (props: any) => {
+  const [task, setTask] = useState("");
+  const [update, setUpdate] = useState(0);
+
+  const handleSubmit = (event: any) => {
+    event.preventDefault();
+    props.submitTask({ todo: task });
+    setUpdate(update + 1);
+  };
+
+  const deleteTask = (taskId: any) => {
+    props.deleteTask(taskId);
+    setUpdate(update + 1);
+  };
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3000/todos")
+      .then((response) => props.setApi(response.data));
+  }, [update]);
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          className="inputtask"
+          onChange={(e) => setTask(e.target.value)}
+        />
+        <Button type="submit" className="togglebtn" endIcon={<SendIcon />}>
+          Add Task
+        </Button>
+      </form>
+      {props.api.map((item: any) => {
+        return (
+          <div className="task" key={item.id}>
+            <h2 className="taskname">{item.todo}</h2>
+            <CancelIcon
+              className="cancelbtn"
+              onClick={() => deleteTask(item.id)}
+            />
+          </div>
+        );
+      })}
+    </div>
+  );
+};
